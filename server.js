142f0011b2469489ae1e58e9f583f3b76bcd2ab3
dnezzar-iut90 <113182098+dnezzar-iut90@users.noreply.github.com>
2023-10-20 16:29:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -44,9 +44,9 @@ app.post("/addTask", async (req, res) => {
 app.put("/moveTaskDone", async (req, res) => {
   const { name, id } = req.body;
   if (name === "todo") {
-    const result = pool.query('UPDATE todo SET status = 1 WHERE id = $1 RETURNING status', [id]);
+    const result = await pool.query('UPDATE todo SET status = 1 WHERE id = $1 RETURNING status', [id]);
   } else {
-    const result = pool.query('UPDATE todo SET status = 0 WHERE id = $1 RETURNING status', [id]);
+    const result = await pool.query('UPDATE todo SET status = 0 WHERE id = $1 RETURNING status', [id]);
   }
   const back = await pool.query('SELECT * FROM todo');
   res.render("index", { todos: back.rows });
